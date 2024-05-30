79904af3b2761b43e4d1f82b828a9a8bb89bb5b6
niahinti <niahinti@gmail.com>
2024-05-30 17:19:47 -0400

    browser fixture, pom, search keywords

--- steps/SoftServeSearch.feature
+++
@@ -1,9 +0,0 @@
-Feature: SoftServeSearch
-
-  Scenario: Search for Academy Page
-    Given I'm on SoftSearch main page
-    When I click on the search icon
-    And I type "Academy" in the search box in Search Page
-    And I click on "IT Academy" search result in Search Page
-    Then I should see "SoftServe Academy" page title
-    And I should see "Empowering learning solutions to start your career in IT." subtitle on SoftServe Academy page
--- steps/test_SoftServeSearchSteps.py
+++ steps/test_SoftServeSearchSteps.py
@@ -1,56 +1,106 @@
 import pytest
+from pytest_bdd import *
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-from pytest_bdd import scenario, given, when, then
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
-@pytest.mark.usefixtures("web_driver")
 
-@pytest.fixture(scope="class")
-def web_driver(request):
+##########################################
+## POMs
+##########################################
+
+class LayoutPage():
+    def __init__(self, driver):
+        self.driver = driver
+
+    def getPageTitle(self):
+        return self.title
+
+    def waitUntilElementIsVisible(self: object, locator: tuple):
+        return WebDriverWait(self, 10).until(EC.visibility_of_element_located(locator))
+
+    def clickSearchBtn(self):
+        self.find_element(By.CSS_SELECTOR, '.menu__search-image').click()
+
+
+class HomePage(LayoutPage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+
+class SearchPage(LayoutPage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def waitUntilPageLoads(self):
+        self.waitUntilElementIsVisible((By.CSS_SELECTOR, '.form-input__text'))
+
+    def inputSearch(self, text):
+        self.find_element(By.CSS_SELECTOR, '.form-input__text').send_keys(text)
+        self.find_element(By.CSS_SELECTOR, '.form-input__text').send_keys(Keys.ENTER)
+
+
+##########################################
+## Step definitions
+##########################################
+
+
+@pytest.fixture
+def browser():
     chr_options = Options()
     chr_options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chr_options)
     driver.get("https://www.softserveinc.com/")
-    yield
-    driver.close()
+    yield driver
+    driver.quit()
 
-@scenario("SoftServeSearch.feature", "Search for Academy Page")
-def test_setUp(web_driver, capsys):
+
+@scenario("../features/SoftServeSearch.feature", "Search for Academy Page")
+def test_setUp(browser):
     pass
 
-@given("I'm on SoftSearch main page")
-def launchBrowser(web_driver, capsys):
-    # driver = web_driver
-    # title = driver.title
-    # with capsys.disabled():
-    # print(title)
-    # SoftServe | Software Development & Digital Services Company
-    with capsys.disabled():
-        print("I'm on SoftSearch main page")
 
-@when('I click on the search icon')
-def click_search_icon(capsys):
-    with capsys.disabled():
-      print("I click on the search icon")
+@given("Im on SoftSearch main page with title 'SoftServe | Software Development & Digital Services Company'")
+def main_page_title(browser):
+    param = 'SoftServe | Software Development & Digital Services Company'
+    print(f"Im on SoftSearch main page with title '{param}'")
+    assert HomePage.getPageTitle(browser) == 'SoftServe | Software Development & Digital Services Company'
 
-@when('I type "Academy" in the search box in Search Page')
-def type_search_term(capsys):
-    with capsys.disabled():
-        print(f"I type 'Academy' in the search box in Search Page")
 
-@when('I click on "IT Academy" search result in Search Page')
-def click_search_result(capsys):
-    with capsys.disabled():
-        print(f'I click on "IT Academy" search result in Search Page')
+@when("I click on the search icon")
+def click_search_icon(browser):
+    print("I click on the search icon")
+    HomePage.clickSearchBtn(browser)
+    SearchPage.waitUntilElementIsVisible(browser, (By.CSS_SELECTOR, '.form-input__text'))
+    assert SearchPage.getPageTitle(browser) == 'Search | SoftServe'
 
-@then('I should see "SoftServe Academy" page title')
-def check_page_title(capsys):
-    with capsys.disabled():
-        print(f'I should see "SoftServe Academy" page title')
 
-@then('I should see "Empowering learning solutions to start your career in IT." subtitle on SoftServe Academy page')
-def check_page_subtitle(capsys):
-    with capsys.disabled():
-        print(f'I should see "Empowering learning solutions to start your career in IT." subtitle on SoftServe Academy page')
+@when("I type 'Academy' in the search box in Search Page")
+def type_search_term(browser):
+    param = 'Academy'  # param would be received from cucumber step but it's failing to recognize step definition
+    print(f"I type '{param}' in the search box in Search Page")
+    SearchPage.inputSearch(browser, param)
+
+
+@when("I click on 'IT Academy' search result in Search Page")
+def click_search_result(browser):
+    param = 'IT Academy'
+    print(f"I click on '{param}' search result in Search Page")
+
+
+@then("I should see 'SoftServe Academy' page title")
+def check_page_title(browser):
+    param = 'SoftServe Academy'
+    print(f"I should see '{param}' page title")
+
+
+@then("I should see 'Empowering learning solutions to start your career in IT.' subtitle on SoftServe Academy page")
+def check_page_subtitle(browser):
+    param = 'Empowering learning solutions to start your career in IT.'
+    print(
+        f"I should see '{param}' subtitle on SoftServe Academy page")
