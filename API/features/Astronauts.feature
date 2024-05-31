42ae2450824859422d1db6c92f914c3037c93a7f
niahinti <niahinti@gmail.com>
2024-05-31 14:49:36 -0400

    astro parameters

--- API/features/Astronauts.feature
+++ API/features/Astronauts.feature
@@ -6,7 +6,7 @@ Feature: Astronauts
   Scenario Outline: Verify astronaut is not on the ISS
     When I send a GET request
     Then I should get a status code "200"
-    And Name <name> should not be on the <station> station
+    And Name "<name>" should not be on the "<station>" station
 
     Examples:
       | name             | station  |
--- API/steps/test_ISS_Astros.py
+++ API/steps/test_ISS_Astros.py
@@ -4,13 +4,13 @@ import requests
 from pytest_bdd import scenarios, given, when, then, parsers
 from conftest import global_state, response
 
-@scenario("../features/Astronauts.feature", "Verify astronaut is not on the ISS")
 
-def test_setUpAstros(global_state, response):
+@scenario("../features/Astronauts.feature", "Verify astronaut is not on the ISS")
+def test_setUp(global_state, response):
     pass
 
 
-@given(parsers.parse("The url to get astronaut information is \"{url}\""))
+@given(parsers.parse("The url to get austronaut information is \"{url}\""))
 def set_api_url(global_state, url):
     global_state['api_url'] = url
 
@@ -22,14 +22,16 @@ def send_get_request(global_state, response):
     response['data'] = resp.json()
 
 
-@then(parsers.parse("I should get a status code \"200\""))
-def check_status_code(response):
-    assert response['status_code'] == 200
+@then(parsers.parse("I should get a status code \"{code}\""))
+def check_status_code(response, code):
+    assert response['status_code'] == int(code)
 
 
-@then(parsers.parse("Name <name> should not be on the <station> station"))
+@then(parsers.parse("Name \"{name}\" should not be on the \"{station}\" station"))
 def verify_astronaut_not_on_station(response, name, station):
     astronauts = response['data']['people']
+    astronauts.sort(key=lambda x: x['name'].split()[-1])
     for astronaut in astronauts:
+        print(astronaut)
         if astronaut['name'] == name and astronaut['craft'] == station:
             pytest.fail(f"{name} is on the {station} station")
